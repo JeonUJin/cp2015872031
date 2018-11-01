86a443f66761de7b60289dec45e9eb9e7c60279f
2015872031 <dn670528@naver.com>
2018-11-01 22:38:56 +0900

    20181030

--- 20181023/20181023/함수.cpp
+++ 20181023/20181023/함수.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int gVar = 100; //전역변수(global variable)
+//int gVar = 100; //전역변수(global variable)
 //함수
 //추상화 ==> 목적, 요약
 
@@ -10,10 +10,11 @@ int gVar = 100; //전역변수(global variable)
 //출력 : 두 정수의 합
 //부수효과 : 없음
 
-int sum(int a, int b)////////<-함수선언
+/*int sum(int a, int b)////////<-함수선언
 { 
 	static int c; //지역변수
-	c = a + b; a = 100; //a = 100일 때 얘가 왜 반응 안함?
+	c = a + b; 
+	a = 100; //a = 100일 때 얘가 왜 반응 안함?
 	return c;
 } ///////함수 본체
 ////////////////여기까지가 함수선언
@@ -39,3 +40,45 @@ int main() {
 	//return 0;
 }
 //디버거 두개의 표시! 점으로 들어가는거 : step into 랑 점 밖으로 돌아가는거 : step over
+*/
+
+/*int sum(int a, int b){
+	int c;
+	c = a + b;
+	return c;
+}
+
+void goodPrint(){
+	printf("good\n");
+}
+
+int main(){
+	int x = 10, y = 30, z;
+	z = sum(x,y);
+	printf("z = %d, x = %d\n", z, x);
+	goodPrint();
+
+}
+*/
+
+int sum(int a, int b){
+	static int c;
+	c = a + b;
+	return c;
+}
+
+void goodPrint(){
+	printf("good\n");
+}
+
+int main(){
+	int x = 100, y = 250, z;
+	z = sum(x,y);
+	printf("x = %d, z = %d\n", x, z);
+	goodPrint();
+}
+
+
+
+
+
---
+++ 20181030/20181030/배열.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+//함수 printArray()
+//입력 : 배열
+//추력 : 없음
+//부수효과 : 화면이 모든 요소를 출력
+void printArray(int arr[], int length){
+	int count = 0;
+	while (count < length){
+		printf("a[%d] = %d\n", count, arr[count]);
+		count++;
+	}
+}
+
+int main(){
+	int len = 4;
+	//int a[4];//배열선언
+	//1. 배열의 이름 : a
+	//2. 각 요소의 자료형 : int
+	//3. 배열의 길이 : 4
+	/*a[0] = 20;//literal도 쓸 수 있고 변수를 쓸 수도 있음. 지금 같은 경우는 []내부에 리터럴 값 들어가있음
+	a[1] = 200;
+	a[2] = 2000;
+	a[3] = 20000;
+	*/
+	int a[4] = {2, 20, 200, 2000};//배열 초기화 방법
+	printArray(a, len);//배열을 매개변수로 (배열이름 a, 배열길이 lengh)
+	printf("%d\n", a[100]);
+
+	int arr[] = {3, 30, 300};//길이 3의 배열 생성
+	int count = 0;
+	while (count < len){
+	
+	printf("a[%d] = %d\n", count, a[count]);//[]안에 count가 들어가있음 이 경우는 변수가 쓰인 경우
+	count++;
+	}
+
+	//배열을 매개변수로 함수에 던지는 방법
+
+
+	return 0;
+}
